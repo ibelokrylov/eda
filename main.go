1d2ee227621d4e5afe85c2d57487817cd46e490f
Ivan Belokrylov <bellivan64@gmail.com>
2024-06-15 19:22:57 +0300

    [upd]: cors

--- main.go
+++ main.go
@@ -17,8 +17,10 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://dev-eda.ibell.online, http://localhost:3000",
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
 		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
 	config.Init()
